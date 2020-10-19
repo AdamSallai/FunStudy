8da610180e11b5031d6403980b47cc3cacdf6e84
eni-kliszki <kliszkieniko@gmail.com>
2020-10-19 17:39:49 +0200

    basic smooth scroll

--- src/components/LearningSite.js
+++ src/components/LearningSite.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function LearningSite() {
     return (
-        <div>
-            learning
-        </div>
+        <Learning id="learning_site">
+            <h1>Learning</h1>
+            <div>Enter difficulty</div>
+            <div>START</div>
+        </Learning>
     )
 }
+
+const Learning = styled.div`
+    margin: 300px 0px;
+`
