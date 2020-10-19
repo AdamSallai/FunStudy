dc3140a515d59b9410c54c32610d674c7585d03e
Adam Sallai <yadam1998@gmail.com>
2020-10-19 15:29:05 +0200

    added styled navbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import MemoryGame from './components/MemoryGame';
-import LearningSite from './components/LearningSite';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import MemoryGame from "./components/MemoryGame";
+import LearningSite from "./components/LearningSite";
+import { createGlobalStyle } from "styled-components";
+
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={About}
-        />
-        <Route
-          exact
-          path="/learning_site"
-          component={LearningSite}
-        />
-        <Route
-          exact
-          path="/memory"
-          component={MemoryGame}
-        />
-      </Switch>
+      <Route exact path="/" component={About} />
+      <Route exact path="/learning_site" component={LearningSite} />
+      <Route exact path="/memory" component={MemoryGame} />
     </Router>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+`;
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
-import React from 'react';
-
+import React from "react";
 
 export default function About() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <h1>About</h1>
+      <p>It is an easy and fun way to learn words and expand your vocabulary</p>
+      <h2>Learning:</h2>
+      <p>
+        This is the best way to memorize new expressions by associating words
+        with pictures and descriptions. You get a picture or description and
+        your goal is to guess the right word.
+      </p>
+      <h2>Memory game:</h2>
+      <p>
+        Memory game is a card game in which all of the cards are laid face down
+        on a surface and two cards are flipped face up over each turn. The
+        object of the game is to turn over pairs of matching cards. One of the
+        cards contains a word (easy mode) or explanation (hard mode), and the
+        other one is a picture of that word or explanation. Your goal is to find
+        all of the pairs before the time is out.
+      </p>
+    </div>
+  );
 }
