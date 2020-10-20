ec755f0c9953406ae4cde8dd25bdbc82ffd3d2fb
eni-kliszki <kliszkieniko@gmail.com>
2020-10-20 13:57:05 +0200

    basic card

--- src/components/LearningData.js
+++ src/components/LearningData.js
@@ -5,39 +5,51 @@ export default function LearningData() {
   const randomWords = require("random-words");
   const Owlbot = require("owlbot-js");
   var client = Owlbot("d9babcf6a7b2f35f7cf176123956ef6dbe4b5585");
-  let [card, setCard] = useState();
-  let [isLoading, setIsLoading] = useState(true);
+  const [card, setCard] = useState();
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   let word;
 
   useEffect(() => {
     word = randomWords(1);
-    console.log(word);
     setIsLoading(true);
+    setErr(false);
     client.define(word)
-    .then(function (result) {
+      .then(result => {
         console.log(result);
         setCard(result);
         setIsLoading(false);
       })
-    .catch((err) => {
-        console.log(err);
-        word = randomWords(1);
-      });    
-  }, [word]);
+      .catch((e) => {
+        console.log("eh");
+        setErr(true);
+      });
+  }, [err]);
 
-  if(isLoading) {
-      return( 
-      <div>Loading...</div>
-      );
-  } else {
+  if (isLoading) {
     return (
-        <div className="card">
-            {card.definitions.map((item) => 
-            (item.image_url && <img src ={item.image_url}></img>)
-            )
-            }
+      <div>Loading...</div>
+    );
+  } else {
+    let image = null;
+    card.definitions.forEach(item => {
+      if (item.image_url !== null) {
+        image = item.image_url;
+      }
+    })
+    return (
+      <div className="card">
         <p>{card.word}</p>
-        </div>
+        <img src={image} alt="" />
+        <p>{card.definitions[0].definition}</p>
+
+        <form action="/learning">
+          <label htmlFor="answer">Your guess: </label>
+            <input type="text"></input>
+          <button type="submit">Guess</button>
+      </form>
+
+      </div>
     );
   }
 }
