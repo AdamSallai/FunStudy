23b7e8bb481b5da8fae308861cadcdfcc32ead9e
Adam Sallai <yadam1998@gmail.com>
2020-11-04 11:02:50 +0100

    memory game api request endpoint changed to own backend

--- src/components/GetCardData.js
+++ src/components/GetCardData.js
@@ -1,16 +1,8 @@
-export default function GetCardData(callback) {
-  const randomWords = require("random-words");
-  const Owlbot = require("owlbot-js");
-  var client = Owlbot("d9babcf6a7b2f35f7cf176123956ef6dbe4b5585");
-
-  const word = randomWords(1);
-  client.define(word).then((result) => {
-    callback(false);
-    return result;
-  });
-}
-
-
-export function getRandomWord() {
-  return require("random-words");
+export default function getCardData(setCard, setIsLoading) {
+  fetch("http://localhost:8080/card")
+    .then((res) => res.json())
+    .then((data) => {
+       setCard(data);
+       setIsLoading(false);
+    });
 }
