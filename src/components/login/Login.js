37067c020dc5d4cedf16a1a5d4a3caf012e6822e
Adam Sallai <yadam1998@gmail.com>
2020-11-05 15:19:42 +0100

    add login field placeholders to main page

---
+++ src/components/login/Login.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+
+export default function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login =  async () => {
+    const requestBody = {
+      "email": email,
+      "password": password
+    }
+
+    console.log(requestBody);
+
+    
+    fetch('http://localhost:8080/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+    }).then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+    
+  }
+    
+
+  return (
+    <div id="login" className="authentication-box">
+        <h2>Login</h2>
+        <div className="authentication-textbox">
+            <input type="text" placeholder="E-mail" name="email" autoComplete="off" required onChange={e => setEmail(e.target.value)}></input>
+            <input type="password" placeholder="Password" name="password" autoComplete="off" required onChange={e => setPassword(e.target.value)}></input>
+            <button className="authentication-button" type="submit" onClick={login}>Sign Up</button>
+        </div>
+    </div>    
+  )
+}
