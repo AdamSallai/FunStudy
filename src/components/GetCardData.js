c3e44e5f4e6e84970d2479cf1adc0b01d93e76af
Eszkis <laszlo.szkis@gmail.com>
2020-11-19 14:05:56 +0100

    learning game useing new endpoints depending on difficulty

--- src/components/GetCardData.js
+++ src/components/GetCardData.js
@@ -6,3 +6,23 @@ export default function getCardData(setCard, setIsLoading) {
        setIsLoading(false);
     });
 }
+
+function getCardDataWithOutPicture(setCard, setIsLoading) {
+  fetch("http://localhost:8080/card-without-picture")
+    .then((res) => res.json())
+    .then((data) => {
+       setCard(data);
+       setIsLoading(false);
+    });
+}
+
+function getCardDataWithPicture(setCard, setIsLoading) {
+  fetch("http://localhost:8080/card-with-picture")
+    .then((res) => res.json())
+    .then((data) => {
+       setCard(data);
+       setIsLoading(false);
+    });
+}
+
+export{getCardDataWithOutPicture,getCardDataWithPicture}
