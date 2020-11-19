fe9de5332fdcb22e0b22f1f66d299ae0b68d619a
Kattkovacs <kattkovacs@gmail.com>
2020-11-19 14:28:35 +0100

    logged in email to session

--- src/components/GetCardData.js
+++ src/components/GetCardData.js
@@ -1,8 +1,8 @@
 export default function getCardData(setCard, setIsLoading) {
-  fetch("http://localhost:8080/card")
+  fetch("http://localhost:8080/card-with-picture")
     .then((res) => res.json())
     .then((data) => {
-       setCard(data);
-       setIsLoading(false);
+      setCard(data);
+      setIsLoading(false);
     });
 }
